d662f1c2e818163ce7021c71f4fcb7da11737549
vibinab <abvibin4@gmail.com>
2022-07-29 13:42:38 +0530

    logout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import AuthContext from './store/auth-context';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
@@ -6,17 +8,30 @@ import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 
 function App() {
+
+  const authctx=useContext(AuthContext)
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!authctx.isLoggedIn && 
+        (<Route path='/auth'>
           <AuthPage />
         </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        )}
+       
+          <Route path='/profile'>
+          { authctx.isLoggedIn && 
+          <UserProfile />}
+          {!authctx.isLoggedIn && <Redirect to='/auth' />}
+          
+        </Route>
+      
+
+        <Route path="*">
+          <Redirect to="/"></Redirect>
         </Route>
       </Switch>
     </Layout>
